327828d1ba10a2bdcc4a10c46547a5de14d16a77
TWang <wang.tw.ted@gmail.com>
2024-05-01 03:22:49 +0800

    coding style

--- lab07/main_demo1_2.cpp
+++ lab07/main_demo1_2.cpp
@@ -12,10 +12,11 @@ static BufferedSerial device1(D10, D9); // tx, rx  D10:tx  D9:rx
 static BufferedSerial device2(D1, D0);  // tx, rx   D1:tx   D0:rx
 static BufferedSerial serial_port(USBTX, USBRX);
 
-void master_thread() {
-    while(1)
+void master_thread()
+{
+    while (1)
     {
-        if (device2.readable()) 
+        if (device2.readable())
         {
             int receive;
             device2.read(&receive, 1);
@@ -24,16 +25,19 @@ void master_thread() {
     }
 }
 
-void slave_thread() {
+void slave_thread()
+{
     int number = 0;
-    while (number < 100) {
+    while (number < 100)
+    {
         device1.write(&number, 1);
-        number ++;
+        number++;
         ThisThread::sleep_for(100ms);
     }
 }
 
-int main() {
+int main()
+{
     // Set desired properties (9600-8-N-1).
     device1.set_baud(9600);
     device1.set_format(
