02a7a8b74cca65cc8582a830601d29609b3efbfe
TWang <wang.tw.ted@gmail.com>
2024-05-16 15:41:24 +0800

    codeing style

--- lab09/main_9_demo2.cpp
+++ lab09/main_9_demo2.cpp
@@ -18,8 +18,8 @@ LCD lcd(D10, D9, D8, D11, D13, D12);
 // Touch             tclk,  tcs,          tdin,  dout,  irq
 // mbed (L4S5I)PMOD. PD_1,  PD_2 (Buttom) PD_3,  PD_4,  PD_5
 UTouch myTouch(PD_1, PD_2, PD_3, PD_4, PD_5);
-#define LVGL_TICK 5// Time tick value for lvgl in 5ms (1-10ms)
-const microseconds TICKER_TIME =  LVGL_TICK * 1ms; // modified to miliseconds 5000us=5ms
+#define LVGL_TICK 5                               // Time tick value for lvgl in 5ms (1-10ms)
+const microseconds TICKER_TIME = LVGL_TICK * 1ms; // modified to miliseconds 5000us=5ms
 
 //---------------------------------------------------------------------------------------------
 // lvgl Setup
@@ -57,37 +57,40 @@ float waveform[waveformLength] = { // 128 samples of a sine waveform
 
 Mutex dac_mutex;
 
-void playNote(int freq, float vol, bool isPoen) {
+void playNote(int freq, float vol, bool isPoen)
+{
     if (!isPoen)
         return;
     int j = waveformLength;
     int waitTime = 1000000 / waveformLength / freq;
-    int i = 0.3*freq; //play i iterations of waveform samples
-    //printf("Sample iteration=%d\n", i);
+    int i = 0.3 * freq; // play i iterations of waveform samples
+    // printf("Sample iteration=%d\n", i);
     printf("Play notes %d\n", freq);
-    dac_mutex.lock(); //To prevent other calls to playNote to use DAC
-    while (i--) {
+    dac_mutex.lock(); // To prevent other calls to playNote to use DAC
+    while (i--)
+    {
         j = waveformLength;
-        while (j--) {
-        Aout = waveform[j]*vol; //scale with volume
-        wait_us(waitTime);
+        while (j--)
+        {
+            Aout = waveform[j] * vol; // scale with volume
+            wait_us(waitTime);
         }
     }
     dac_mutex.unlock();
 }
 
-//EventQueue for calling playNote()
+// EventQueue for calling playNote()
 EventQueue queue(32 * EVENTS_EVENT_SIZE);
 Thread t;
-const int notes=8; //support 8 notes
-int id[notes]; //create an thread ID for each note
+const int notes = 8; // support 8 notes
+int id[notes];       // create an thread ID for each note
 
-string NoteName[]={"C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5"};
-//frequency table for C4, D4, E4, F4, G4, A4, B4, C5
-const float frequency[]={261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25};
+string NoteName[] = {"C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5"};
+// frequency table for C4, D4, E4, F4, G4, A4, B4, C5
+const float frequency[] = {261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88, 523.25};
 
-
-int main(void) {
+int main(void)
+{
     printf("Touch keyboard demo\n");
     display_init();
     touchpad_init();
@@ -101,14 +104,15 @@ int main(void) {
     queue_lvgl.call_every(5ms, lv_task_handler); // 5ms should match LVGL_TICK 5
     lv_demo_widgets();
     t.start(callback(&queue, &EventQueue::dispatch_forever));
-
 }
 
-void lv_ticker_func(void) {
+void lv_ticker_func(void)
+{
     lv_tick_inc(LVGL_TICK);
 }
 
-void display_init(void) {
+void display_init(void)
+{
     // Init the touch screen display
 
     lcd.InitLCD();
@@ -116,11 +120,11 @@ void display_init(void) {
 
     lv_init(); // Initialize the LittlevGL
     static lv_disp_buf_t disp_buf;
-    //Double buffering
+    // Double buffering
     static lv_color_t buf1[LV_HOR_RES_MAX * 48]; // Declare a buffer for 48 lines (1/10 of vertical resoluion)
     static lv_color_t buf2[LV_HOR_RES_MAX * 48]; // Declare a buffer for 48 lines (1/10 of vertical resoluion) (ping-pong buffer)
     lv_disp_buf_init(&disp_buf, buf1, buf2,
-                    LV_HOR_RES_MAX * 48); // Initialize the display buffer
+                     LV_HOR_RES_MAX * 48); // Initialize the display buffer
 
     // Implement and register a function which can copy a pixel array to an area
     // of your display
@@ -132,7 +136,8 @@ void display_init(void) {
 }
 
 void my_disp_flush_cb(lv_disp_drv_t *disp_drv, const lv_area_t *area,
-                       lv_color_t *color_p) {
+                      lv_color_t *color_p)
+{
     // cast lv_color_t to uint16_t pointer
     uint16_t *colors = reinterpret_cast<uint16_t *>(color_p);
     // Put all pixels to the screen one-by-one by FillArea for a specified area
@@ -141,25 +146,31 @@ void my_disp_flush_cb(lv_disp_drv_t *disp_drv, const lv_area_t *area,
     lv_disp_flush_ready(disp_drv);
 }
 
-void touchpad_init(void) {
+void touchpad_init(void)
+{
     myTouch.InitTouch();
     myTouch.SetPrecision(PREC_MEDIUM);
-    lv_indev_drv_t indev_drv;      // Descriptor of an input device driver
-    lv_indev_drv_init(&indev_drv); // Basic initialization
+    lv_indev_drv_t indev_drv;               // Descriptor of an input device driver
+    lv_indev_drv_init(&indev_drv);          // Basic initialization
     indev_drv.type = LV_INDEV_TYPE_POINTER; // The touchpad is pointer type device
     indev_drv.read_cb = touchpad_read;      // Set the touchpad_read function
     lv_indev_drv_register(&indev_drv);      // Register touch driver in LvGL
 }
 
-static bool touchpad_read(lv_indev_drv_t *indev, lv_indev_data_t *data) {
+static bool touchpad_read(lv_indev_drv_t *indev, lv_indev_data_t *data)
+{
     // Read your touchpad
-    if (myTouch.DataAvailable()) {
-        if (myTouch.Read()) {
-        data->point.x = myTouch.GetX();
-        data->point.y = myTouch.GetY();
-        data->state = LV_INDEV_STATE_PR;
+    if (myTouch.DataAvailable())
+    {
+        if (myTouch.Read())
+        {
+            data->point.x = myTouch.GetX();
+            data->point.y = myTouch.GetY();
+            data->state = LV_INDEV_STATE_PR;
         }
-    } else {
+    }
+    else
+    {
         data->point.x = 0;
         data->point.y = 0;
         data->state = LV_INDEV_STATE_REL;
@@ -169,37 +180,42 @@ static bool touchpad_read(lv_indev_drv_t *indev, lv_indev_data_t *data) {
 
 lv_obj_t *screenMain;
 lv_obj_t *mbox1;
-lv_obj_t* buttons[notes];
-lv_obj_t* label[notes];
+lv_obj_t *buttons[notes];
+lv_obj_t *label[notes];
 
 int lastIndex = -1;
-static void event_handler(lv_obj_t *obj, lv_event_t event) {
-    int index=-1;
-    //Search matched buttons object index
-    for(int i=0; i<notes; i++){
-        if(obj==buttons[i]){
-            index=i;
+static void event_handler(lv_obj_t *obj, lv_event_t event)
+{
+    int index = -1;
+    // Search matched buttons object index
+    for (int i = 0; i < notes; i++)
+    {
+        if (obj == buttons[i])
+        {
+            index = i;
             break;
         }
     }
     // if (event != LV_EVENT_RELEASED)
-    if (event == LV_EVENT_PRESSING || event == LV_EVENT_PRESSED) 
-    { //Trigger call_every only at the first press
-        string note="Playing "+string(NoteName[index]);
-        //printf(note.c_str());
+    if (event == LV_EVENT_PRESSING || event == LV_EVENT_PRESSED)
+    { // Trigger call_every only at the first press
+        string note = "Playing " + string(NoteName[index]);
+        // printf(note.c_str());
         lv_msgbox_set_text(mbox1, note.c_str());
 
-        if(index!=-1){ //Check if notes index is supported otherwise do nothing
+        if (index != -1)
+        { // Check if notes index is supported otherwise do nothing
             // queue.dispatch();
             // queue.cancel(id[index]);
-            id[index]=queue.call_every(1s, playNote, frequency[index], true, 3.3); //call playNote for 1s once
+            id[index] = queue.call_every(10ms, playNote, frequency[index], true, 3.3); // call playNote for 1s once
             lastIndex = index;
-            //printf("Call playNote index=%d\n", index);
-        }else{
-            queue.cancel(id[index]);
-            //printf("Exception: index=%d\n", index);
+            // printf("Call playNote index=%d\n", index);
         }
-
+        else
+        {
+            queue.cancel(id[index]);
+            // printf("Exception: index=%d\n", index);
+        }
     }
     // else if (event != LV_EVENT_PRESSED) {
     //     // string note="Stop playing "+string(NoteName[index]);
@@ -213,31 +229,37 @@ static void event_handler(lv_obj_t *obj, lv_event_t event) {
     //     //     //printf("Exception: index=%d\n", index);
     //     // }
     // }
-    else if (event == LV_EVENT_RELEASED) {
-        string note="Stop playing "+string(NoteName[index]);
-        //printf(note.c_str());
+    else if (event == LV_EVENT_RELEASED)
+    {
+        string note = "Stop playing " + string(NoteName[index]);
+        // printf(note.c_str());
         lv_msgbox_set_text(mbox1, note.c_str());
 
-        if(index!=-1){ //Check if notes index is supported otherwise do nothing
-            queue.cancel(id[index]); //stop playing a note
-            //printf("Cancel playNote index=%d\n", index);
-        }else{
-            //printf("Exception: index=%d\n", index);
+        if (index != -1)
+        {                            // Check if notes index is supported otherwise do nothing
+            queue.cancel(id[index]); // stop playing a note
+            // printf("Cancel playNote index=%d\n", index);
+        }
+        else
+        {
+            // printf("Exception: index=%d\n", index);
         }
     }
 }
 
-void lv_demo_widgets() {
+void lv_demo_widgets()
+{
 
     screenMain = lv_obj_create(NULL, NULL);
-    for(int i=0; i<notes; i++){
-        buttons[i]=lv_btn_create(screenMain, NULL);
+    for (int i = 0; i < notes; i++)
+    {
+        buttons[i] = lv_btn_create(screenMain, NULL);
         lv_obj_set_event_cb(buttons[i], event_handler);
         label[i] = lv_label_create(buttons[i], NULL);
         lv_label_set_text(label[i], NoteName[i].c_str());
         lv_label_set_align(label[i], LV_LABEL_ALIGN_CENTER);
         lv_obj_set_size(buttons[i], 240, 36);
-        lv_obj_align(buttons[i], NULL, LV_ALIGN_IN_BOTTOM_MID, 0,-100-40*i);
+        lv_obj_align(buttons[i], NULL, LV_ALIGN_IN_BOTTOM_MID, 0, -100 - 40 * i);
     }
 
     mbox1 = lv_msgbox_create(screenMain, NULL);
