6321a2215d26dd299b209c4c38f89c78f0c710b7
TWang <wang.tw.ted@gmail.com>
2024-05-28 18:35:27 +0800

    update

--- final_project/bbcar-main/parallax_servo.cpp
+++ final_project/bbcar-main/parallax_servo.cpp
@@ -1,6 +1,7 @@
 #include "parallax_servo.h"
 
-parallax_servo::parallax_servo (PwmOut& pin_control, PwmIn& pin_feedback) {
+parallax_servo::parallax_servo(PwmOut &pin_control, PwmIn &pin_feedback)
+{
     pin_control.period(0.02);
     pwmOut = &pin_control;
     factor = 1;
@@ -11,64 +12,70 @@ parallax_servo::parallax_servo (PwmOut& pin_control, PwmIn& pin_feedback) {
 }
 
 // set new target pwm value (speed = pwm value)
-void parallax_servo::set_speed( double value ) {
+void parallax_servo::set_speed(double value)
+{
     target_pwm_value = value * factor;
     // you can determine ramping factor formula by yourself
-    ramping_factor = abs(target_pwm_value - current_pwm_value) / 50;
-    if (target_pwm_value > 200) target_pwm_value = 200;
-    else if (target_pwm_value < -200) target_pwm_value = -200;
+    ramping_factor = abs(target_pwm_value - current_pwm_value) / 5;
+    if (target_pwm_value > 200)
+        target_pwm_value = 200;
+    else if (target_pwm_value < -200)
+        target_pwm_value = -200;
 }
 
-void parallax_servo::set_factor( double value ){
+void parallax_servo::set_factor(double value)
+{
     factor = value;
 }
 
 // control servo input pwm, also ramping is mainly done in here
-void parallax_servo::control(){
-    if (current_pwm_value > target_pwm_value) {
+void parallax_servo::control()
+{
+    if (current_pwm_value > target_pwm_value)
+    {
         if (current_pwm_value < target_pwm_value + ramping_factor)
             current_pwm_value = target_pwm_value;
-        else current_pwm_value -= ramping_factor;
+        else
+            current_pwm_value -= ramping_factor;
     }
-    else if (current_pwm_value < target_pwm_value) {
+    else if (current_pwm_value < target_pwm_value)
+    {
         if (current_pwm_value > target_pwm_value - ramping_factor)
             current_pwm_value = target_pwm_value;
-        else current_pwm_value += ramping_factor;
+        else
+            current_pwm_value += ramping_factor;
     }
     pwmOut->write((CENTER_BASE + current_pwm_value) / 20000);
 }
 
 // detect the current agle of  feedback 360 servo
-void parallax_servo::feedback360()                          // Position monitoring
-{                           
+void parallax_servo::feedback360() // Position monitoring
+{
     tCycle = pwmIn->period();
 
     int dc = dutyScale * pwmIn->dutycycle();
-    theta = (unitsFC - 1) -                   // Calculate angle
-            ((dc - dcMin) * unitsFC) 
-                  / (dcMax - dcMin + 1);
-    if(theta < 0)                             // Keep theta valid
-        theta = 0; 
-    else if(theta > (unitsFC - 1)) 
+    theta = (unitsFC - 1) - // Calculate angle
+            ((dc - dcMin) * unitsFC) / (dcMax - dcMin + 1);
+    if (theta < 0) // Keep theta valid
+        theta = 0;
+    else if (theta > (unitsFC - 1))
         theta = unitsFC - 1;
 
-            // If transition from quadrant 4 to  
-          // quadrant 1, increase turns count. 
-    if((theta < q2min) && (thetaP > q3max))
+    // If transition from quadrant 4 to
+    // quadrant 1, increase turns count.
+    if ((theta < q2min) && (thetaP > q3max))
         turns++;
-          // If transition from quadrant 1 to  
-          // quadrant 4, decrease turns count. 
-    else if((thetaP < q2min) && (theta > q3max))
-        turns --;
+    // If transition from quadrant 1 to
+    // quadrant 4, decrease turns count.
+    else if ((thetaP < q2min) && (theta > q3max))
+        turns--;
 
-            // Construct the angle measurement from the turns count and
-          // current theta value.
-    if(turns >= 0)
+    // Construct the angle measurement from the turns count and
+    // current theta value.
+    if (turns >= 0)
         angle = (turns * unitsFC) + theta;
-    else if(turns <  0)
+    else if (turns < 0)
         angle = ((turns + 1) * unitsFC) - (unitsFC - theta);
 
-      thetaP = theta;                               // Theta previous for next rep
+    thetaP = theta; // Theta previous for next rep
 }
-
-
--- final_project/mian.cpp
+++ final_project/mian.cpp
@@ -19,89 +19,124 @@ BusInOut qti_pin(D4, D5, D6, D7);
 
 void GoCertainDistance(float distance)
 {
-    car.goCertainDistance(distance); // 6.5*3.14*3
+   car.goCertainDistance(distance); // 6.5*3.14*3
 
-    while (car.checkDistance(1))
-    {
-        if (LaserPingNavigation())
-            break;
-        ThisThread::sleep_for(500ms);
-    }
-    car.stop();
-    ThisThread::sleep_for(3s);
-    printf("error distance = %f\n", (car.servo0.targetAngle - car.servo0.angle) * 6.5 * 3.14 / 360);
+   while (car.checkDistance(1))
+   {
+      if (LaserPingNavigation())
+         break;
+      ThisThread::sleep_for(500ms);
+   }
+   car.stop();
+   ThisThread::sleep_for(3s);
+   printf("error distance = %f\n", (car.servo0.targetAngle - car.servo0.angle) * 6.5 * 3.14 / 360);
 }
 int LaserPingNavigation()
 {
-    parallax_laserping ping1(pin8);
-    if ((float)ping1 > 10)
-    {
-        led1 = 1;
-        return 0;
-    }
-    else
-    {
-        led1 = 0;
-        car.stop();
-        return 1;
-    }
+   parallax_laserping ping1(pin8);
+   if ((float)ping1 > 10)
+   {
+      led1 = 1;
+      printf("no wall");
+      return 0;
+   }
+   else
+   {
+      led1 = 0;
+      printf("has wall");
+      car.stop();
+      return 1;
+   }
 }
 void QTInavigation()
 {
-    parallax_qti qti1(qti_pin);
-    int pattern = 0b0110;
-    car.goStraight(50);
+   parallax_qti qti1(qti_pin);
+   int pattern = 0b0110;
+   car.goStraight(50);
 
-    while (pattern != 0b0000)
-    {
-        pattern = (int)qti1;
-        printf("%d\n", pattern);
+   while (pattern != 0b0000)
+   {
+      pattern = (int)qti1;
+      printf("%d\n", pattern);
 
-        switch (pattern)
-        {
-        case 0b1000:
-            car.turn(50, 0.1);
-            break;
-        case 0b1100:
-            car.turn(50, 0.5);
-            break;
-        case 0b0100:
-            car.turn(50, 0.7);
-            break;
-        case 0b0110:
-            car.goStraight(50);
-            break;
-        case 0b0010:
-            car.turn(50, -0.7);
-            break;
-        case 0b0011:
-            car.turn(50, -0.5);
-            break;
-        case 0b0001:
-            car.turn(50, -0.1);
-            break;
-        case 0b1111:
-            car.stop();
-            break;
-        default:
-            car.goStraight(50);
-        }
-        ThisThread::sleep_for(10ms);
-    }
-    return;
+      switch (pattern)
+      {
+      case 0b1000:
+         car.turn(50, 0.1);
+         break;
+      case 0b1100:
+         car.turn(50, 0.5);
+         break;
+      case 0b0100:
+         car.turn(50, 0.7);
+         break;
+      case 0b0110:
+         car.goStraight(50);
+         break;
+      case 0b0010:
+         car.turn(50, -0.7);
+         break;
+      case 0b0011:
+         car.turn(50, -0.5);
+         break;
+      case 0b0001:
+         car.turn(50, -0.1);
+         break;
+      case 0b1111:
+         car.stop();
+         break;
+      default:
+         car.goStraight(50);
+      }
+      ThisThread::sleep_for(10ms);
+   }
+   return;
 }
 
 int main()
 {
-    // simple test
-    car.goStraight(200);
-    ThisThread::sleep_for(5s);
-    car.stop();
-    ThisThread::sleep_for(5s);
+   wait(1);
+   car.goStraight(30);
+   wait(2.5);
+   car.stop();
+   wait(1);
+   car.rotate(60);
+   wait(0.7);
+   car.stop();
+   wait(1);
+   car.goStraight(30);
+   wait(2.5);
+   car.stop();
+   wait(1);
+   car.rotate(60);
+   wait(0.7);
+   car.stop();
+   wait(1);
+   car.goStraight(30);
+   wait(2.5);
+   car.stop();
+   wait(1);
+   car.rotate(60);
+   wait(0.7);
+   car.stop();
+   wait(1);
+   car.goStraight(30);
+   wait(2.5);
+   car.stop();
+   wait(1);
+   car.rotate(60);
+   wait(0.7);
+   car.stop();
+   wait(1);
+   // simple test
+   car.goStraight(200);
+   ThisThread::sleep_for(5s);
+   car.stop();
+   ThisThread::sleep_for(5s);
 
-    // go certain distance
-    GoCertainDistance(61.2);
+   // go certain distance
+   GoCertainDistance(61.2);
 
-    // QTI Line Following Kit
-    QTInavigation();
+   // QTI Line Following Kit
+   QTInavigation();
 }
