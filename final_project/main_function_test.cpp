1b9e2ed61a9d22bb79d71e131e6ebdef96651198
TWang <wang.tw.ted@gmail.com>
2024-06-07 02:11:13 +0800

    revise

--- final_project/main_function_test.cpp
+++ final_project/main_function_test.cpp
@@ -12,6 +12,7 @@ InterruptIn button(BUTTON1);
 DigitalInOut pin8(D8);
 BusInOut qti_pin(D4, D5, D6, D7);
 bool level = false;
+int hint;
 
 void toggle()
 {
@@ -22,77 +23,76 @@ void FeedbackWheel()
     traveledPath = car.Dest();
     printf("path: %f\n", traveledPath);
 }
+void LaserPin()
+{
+    if ((float)ping1 > 10)
+    {
+        printf("runnung\n");
+        led1 = 1;
+    }
+    else
+    {
+        led1 = 0;
+        printf("stop\n");
+        car.stop();
+        break;
+    }
+    ThisThread::sleep_for(10ms);
+}
+void GoCertainDistance()
+{
+    car.goCertainDistance(21.05); // 6.7*3.14*3
+    while (car.checkDistance(0.5))
+    {
+        ThisThread::sleep_for(500ms);
+    }
+    car.stop();
+
+    ThisThread::sleep_for(3s);
+    printf("error distance = %f\n", (car.servo0.targetAngle + car.servo0.angle) * 6.7 * 3.1416 / 360);
+}
 int main()
 {
     parallax_laserping ping1(pin8);
     parallax_qti qti1(qti_pin);
-    button.rise(&toggle);
     int pattern;
     int goalDistance = 20;
+    button.rise(&toggle);
     car.initPathDist();
-    // car.goStraight(50);
-    // while(1) {
-    //     if((float)ping1 > 10) {
-    //         printf("runnung\n");
-    //         led1 = 1;
-    //     }
-    //     else {
-    //         led1 = 0;
-    //         printf("stop\n");
-    //         car.stop();
-    //         break;
-    //     }
-    //     ThisThread::sleep_for(10ms);
-    // }
-
-    // if (level)
-    // {
-    //     car.goCertainDistance(21.05);   //6.7*3.14*3
-
-    //     while(car.checkDistance(0.5)){
-    //         ThisThread::sleep_for(500ms);
-    //     }
-    //     car.stop();
-
-    //     ThisThread::sleep_for(3s);
-    //     printf("error distance = %f\n", (car.servo0.targetAngle + car.servo0.angle)*6.7*3.1416/360);
-    // }
+    car.goStraight(50);
     while (1)
     {
-        car.goStraight(50);
         if (level)
         {
-            int hint;
             pattern = qti1;
             printf("%u\n", pattern);
-
-            // switch (pattern) {
-            //     case 0b1000: car.turn(40, 0.05); printf("1000\n"); break;
-            //     case 0b1100: car.turn(55, 0.35); printf("1100\n"); break;
-            //     case 0b0100: car.turn(60, 0.55); printf("0100\n"); break;
-            //     case 0b0110: car.turn(65, 0.75); printf("0110\n"); break;
-            //     case 0b0010: car.turn(60, -0.55); printf("0010\n"); break;
-            //     case 0b0011: car.turn(55, -0.35); printf("0011\n"); break;
-            //     case 0b0001: car.turn(40, -0.05); printf("0001\n"); break;
-            //     case 0b0111: hint = 0b0111; printf("0111\n"); break;
-            //     case 0b1110: hint = 0b0111; printf("0111\n"); break;
-            //     case 0b1111:
-            //     {
-            //         if (hint == 0b0111)
-            //             car.turn(50, 0.05);
-            //         else if (hint == 0b1110)
-            //             car.turn(50, -0.05);
-            //         else
-            //             car.stop(); printf("1111\n"); break;
-            //     }
-            //     default: car.stop();
-            // }
-            ThisThread::sleep_for(10ms);
         }
         else
         {
             printf("%f\n", goalDistance - (float)ping1);
             FeedbackWheel();
         }
+        ThisThread::sleep_for(10ms);
     }
 }
+// switch (pattern) {
+//     case 0b1000: car.turn(40, 0.05); printf("1000\n"); break;
+//     case 0b1100: car.turn(55, 0.35); printf("1100\n"); break;
+//     case 0b0100: car.turn(60, 0.55); printf("0100\n"); break;
+//     case 0b0110: car.turn(65, 0.75); printf("0110\n"); break;
+//     case 0b0010: car.turn(60, -0.55); printf("0010\n"); break;
+//     case 0b0011: car.turn(55, -0.35); printf("0011\n"); break;
+//     case 0b0001: car.turn(40, -0.05); printf("0001\n"); break;
+//     case 0b0111: hint = 0b0111; printf("0111\n"); break;
+//     case 0b1110: hint = 0b0111; printf("0111\n"); break;
+//     case 0b1111:
+//     {
+//         if (hint == 0b0111)
+//             car.turn(50, 0.05);
+//         else if (hint == 0b1110)
+//             car.turn(50, -0.05);
+//         else
+//             car.stop(); printf("1111\n"); break;
+//     }
+//     default: car.stop();
+// }
